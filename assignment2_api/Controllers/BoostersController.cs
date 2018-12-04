a6bb6f8e07616e87eff5e6309d524aa13f6b3411
Burak Kaya <boroppi01@gmail.com>
2018-12-03 23:23:40 -0500

    Complete Web API with CRUD functionality for both database tables.

--- assignment2_api/Controllers/BoostersController.cs
+++ assignment2_api/Controllers/BoostersController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace assignment2_api.Controllers
+﻿namespace assignment2_api.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
     using assignment2_api.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -28,7 +27,7 @@ namespace assignment2_api.Controllers
             return this.db.Boosters.OrderBy(b => b.FullName).ToList();
         }
 
-        //GET: api/boosters/1
+        // GET: api/boosters/1
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
@@ -38,9 +37,62 @@ namespace assignment2_api.Controllers
             {
                 return this.Ok(booster);
             }
+
             return this.NotFound();
         }
 
+        // POST: api/boosters
+        [HttpPost]
+        public ActionResult Post([FromBody] Booster booster)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
 
+            this.db.Boosters.Add(booster);
+            this.db.SaveChanges();
+            return this.CreatedAtAction("Post", booster);
+        }
+
+        // PUT: api/boosters
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Booster booster)
+        {
+            if (this.db.Boosters.Find(id) == null)
+            {
+                return this.NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+
+            this.db.Entry(booster).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            this.db.SaveChanges();
+            return this.NoContent();
+        }
+
+        // DELETE: api/boosters
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var booster = this.db.Boosters.Find(id);
+
+            if (booster == null)
+            {
+                return this.NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+
+            this.db.Boosters.Remove(booster);
+            this.db.SaveChanges();
+            return this.Ok();
+        }
     }
 }
--- assignment2_api/Controllers/CustomersController.cs
+++ assignment2_api/Controllers/CustomersController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace assignment2_api.Controllers
+﻿namespace assignment2_api.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
     using assignment2_api.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -52,17 +51,23 @@ namespace assignment2_api.Controllers
 
             this.db.Customers.Add(customer);
             this.db.SaveChanges();
-            return CreatedAtAction("Post", customer);
+            return this.CreatedAtAction("Post", customer);
         }
 
         // PUT: api/albums/1
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody] Customer customer)
         {
+            if (this.db.Customers.Find(id) == null)
+            {
+                return this.NotFound();
+            }
+
             if (!ModelState.IsValid)
             {
                 return this.BadRequest(ModelState);
             }
+
             this.db.Entry(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             this.db.SaveChanges();
             return this.NoContent();
@@ -70,17 +75,23 @@ namespace assignment2_api.Controllers
 
         // DELETE: api/albums/1
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id, [FromBody] Customer customer)
+        public ActionResult Delete(int id)
         {
+            var customer = this.db.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return this.NotFound();
+            }
+
             if (!ModelState.IsValid)
             {
                 return this.BadRequest(ModelState);
             }
 
-            this.db.Entry(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            this.db.Customers.Remove(customer);
             this.db.SaveChanges();
             return this.Ok();
         }
-
     }
 }
